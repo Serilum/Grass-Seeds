7afac646a0f1b0a7413f41795af8b1f2d62e350b
Rick South <rick@natamus.com>
2024-07-28 07:30:24 +0200

    (v3.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/grassseeds/ModFabric.java
+++ Fabric/src/main/java/com/natamus/grassseeds/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.grassseeds;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
 import com.natamus.grassseeds.events.GrassEvent;
 import com.natamus.grassseeds.util.Reference;
@@ -15,6 +16,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
