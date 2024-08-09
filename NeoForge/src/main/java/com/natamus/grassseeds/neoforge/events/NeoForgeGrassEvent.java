cbe49138a514b377038a5b8860c1f66043b606c8
Rick South <rick@natamus.com>
2024-08-09 08:21:09 +0200

    Grass Seeds source code for 1.21.1.

---
+++ NeoForge/src/main/java/com/natamus/grassseeds/neoforge/events/NeoForgeGrassEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.grassseeds.neoforge.events;
+
+import com.natamus.grassseeds.events.GrassEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeGrassEvent {
+	@SubscribeEvent
+	public static void onDirtClick(PlayerInteractEvent.RightClickBlock e) {
+		GrassEvent.onDirtClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
+	}
+}
